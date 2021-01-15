1e2e9b38b1d9378756ed6a59df450a8c3c1d9aa8
lucio rendine <luciorendine69@gmail.com>
2021-01-15 16:25:16 +0100

    push

--- js/script.js
+++ js/script.js
@@ -3,10 +3,29 @@ function randomNumber(min, max) {
   return result;
 }
 
+function userPrompt() {
+  for (var i = 0; i < 5; i++) {
+    var userNumber = parseInt(prompt('Inserisci un numero alla volta '));
+    // listaNumeriUser.push(userNumber); prova
+  }
+}
+
 var listaNumeriRandom = [];
 for (var i = 0; i < 5; i++) {
   var generated = randomNumber(1, 100);
   listaNumeriRandom.push(generated);
 }
 
-alert(JSON.stringify('ricorda questi numeri' + '  ' + listaNumeriRandom));
+alert(JSON.stringify('ricorda questi numeri, tra 30 secondi me gli ripeti' + '  ' + listaNumeriRandom));
+
+// var listaNumeriUser = []; arrayprova
+
+setTimeout(userPrompt, 3000)
+
+for (var i = 0; i < 5; i++) {
+  if (userNumber === listaNumeriRandom[i]) {
+    alert('ok')
+  } else {
+    alert('no')
+  }
+}
